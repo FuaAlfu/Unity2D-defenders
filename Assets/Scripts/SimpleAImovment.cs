24fc46a9557aa5e65c2c3d4be437d577fba7ad0f
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2021-03-24 19:58:22 +0300

    add

---
+++ Assets/Scripts/SimpleAImovment.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+/// <summary>
+/// 2021.3.24
+/// </summary>
+
+public class SimpleAImovment : MonoBehaviour
+{
+    [SerializeField]
+    Transform target;
+
+    //catch
+    NavMeshAgent agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(target.position);
+    }
+}
