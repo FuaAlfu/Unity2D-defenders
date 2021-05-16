577879da7cf52e5d73d54e1bc1b80d050791ff5b
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2021-05-16 19:51:11 +0300

    add

---
+++ Assets/Scripts/SpritePositionSortingOrder.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 2021.5.16
+/// </summary>
+
+public class SpritePositionSortingOrder : MonoBehaviour
+{
+    [SerializeField] private bool runOnce;
+    private SpriteRenderer spriteRenderer;
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void LateUpdate()
+    {
+        float precisionMultiplier = 5f;
+       spriteRenderer.sortingOrder = (int)(-transform.position.y * precisionMultiplier);
+
+        if(runOnce)
+        {
+            Destroy(this); //the script, not the game object
+        }
+    }
+}
